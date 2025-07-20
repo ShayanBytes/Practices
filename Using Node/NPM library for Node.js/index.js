57f18025dd82bc9cf5057961760575c91ef40679
ShayanBytes <shayan.corpservices@gmail.com>
2025-07-20 12:33:53 +0530

    update

---
+++ Using Node/NPM library for Node.js/index.js
@@ -0,0 +1,5 @@
+// installed sillyname package from npm in dash git commandline
+
+var generateName = require('sillyname');
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`);
